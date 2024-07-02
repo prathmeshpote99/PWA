9b11e057cef71932901ad1a257634d767c3183e6
prathmeshpote99 <poteprathmesh@gmail.com>
2024-07-02 14:09:59 +0530

    Feat: add code for use button for installing the website as a progressive web app but the code is in commented

--- serviceworker.js
+++ serviceworker.js
@@ -17,3 +17,31 @@ self.addEventListener("fetch", function (event) {
     })
   );
 });
+
+
+// for button use this code 👇🏻
+
+// var staticCacheName = "pwa";
+// self.addEventListener("install", function (e) {
+//   e.waitUntil(
+//     caches.open(staticCacheName).then(function (cache) {
+//       return cache.addAll(["/"]);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", function (event) {
+//   console.log(event.request.url);
+
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       return response || fetch(event.request).catch(() => {
+//         console.error("Failed to fetch:", event.request.url);
+//         return new Response("Network error occurred", {
+//           status: 408,
+//           statusText: "Network error occurred"
+//         });
+//       });
+//     })
+//   );
+// });
